deee1dfce0f5da5043f3bd8676142768971bfcfd
Jesús Imitola <jesus.gabriel.imitola.acosta@hotmail.com>
2024-05-13 19:29:26 -0500

    fix: remove duplicate head tags and fix letter spacing on copy

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,9 +20,6 @@ function RootLayout(props: Readonly<RootLayoutProps>) {
   return (
     <html lang="en">
       <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
         <link
           rel="icon"
           type="image/png"
